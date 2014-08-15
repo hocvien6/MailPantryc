01a3c15023a40037bd5ea358573f5a54dc93f79c
hocvien6 <hocvien6.vtict@gmail.com>
2014-08-15 16:53:05 +0700

    Problem resolved!!! Enable counting number of rejected address now

--- lib/pantryc-sqlite.c
+++ lib/pantryc-sqlite.c
@@ -8,7 +8,7 @@
 	char *message = (char*) malloc(sizeof(char) * PANTRYC_SQLITE__SQL_LENGTH)
 
 static void pantryc_sqlite__initialize(char *table, char *column);
-static int pantryc_sqlite__execute(sqlite3* database, const char *sql,
+static int pantryc_sqlite__execute(const char *sql,
 		int (*callback)(void*, int, char**, char**), void *data, char *success,
 		char *failure, char **error);
 
@@ -24,7 +24,7 @@ pBOOL pantryc_sqlite__open_database() {
 
 	if (result) {
 		fprintf(pantryc_global__log_file,
-				"**ERROR**		Cannot open database " PANTRYC_SQLITE__DATABASE "\n");
+				"**ERROR**\tCannot open database " PANTRYC_SQLITE__DATABASE "\n");
 		pantryc_sqlite__database = NULL;
 		return pFALSE;
 	}
@@ -46,12 +46,10 @@ void pantryc_sqlite__insert_rejected_receipt_address(address)
 		" (" PANTRYC_SQLITE__COLUMN_ADDRESS_REJECTED_RECEIPT ")"
 		" values ('%s')", address);
 		sprintf(success,
-				"**NOTE**		Add address '%s' to column \"" PANTRYC_SQLITE__COLUMN_ADDRESS_REJECTED_RECEIPT
-				"\" on table \"" PANTRYC_SQLITE__TABLE_ADDRESS "\"\n", address);
-		sprintf(failure, "**ERROR**		Cannot add rejected receipt address, %s\n",
-				error);
-		pantryc_sqlite__execute(pantryc_sqlite__database, sql, NULL, NULL,
-				success, failure, &error);
+				"Add address '%s' to column \"" PANTRYC_SQLITE__COLUMN_ADDRESS_REJECTED_RECEIPT
+				"\" on table \"" PANTRYC_SQLITE__TABLE_ADDRESS "\"", address);
+		sprintf(failure, "Cannot add rejected receipt address '%s'", address);
+		pantryc_sqlite__execute(sql, NULL, NULL, success, failure, &error);
 	}
 }
 
@@ -70,13 +68,11 @@ void pantryc_sqlite__delete_rejected_receipt_address(address)
 		" where " PANTRYC_SQLITE__COLUMN_ADDRESS_REJECTED_RECEIPT " = '%s';",
 				address);
 		sprintf(success,
-				"**NOTE**		Remove address '%s' from column \"" PANTRYC_SQLITE__COLUMN_ADDRESS_REJECTED_RECEIPT
-				"\" on table \"" PANTRYC_SQLITE__TABLE_ADDRESS "\"\n", address);
-		sprintf(failure,
-				"**ERROR**		Cannot remove rejected receipt address, %s\n",
-				error);
-		pantryc_sqlite__execute(pantryc_sqlite__database, sql, NULL, NULL,
-				success, failure, &error);
+				"Remove address '%s' from column \"" PANTRYC_SQLITE__COLUMN_ADDRESS_REJECTED_RECEIPT
+				"\" on table \"" PANTRYC_SQLITE__TABLE_ADDRESS "\"", address);
+		sprintf(failure, "Cannot remove rejected receipt address '%s'",
+				address);
+		pantryc_sqlite__execute(sql, NULL, NULL, success, failure, &error);
 	}
 }
 
@@ -93,11 +89,11 @@ pBOOL pantryc_sqlite__check_rejected_receipt_address_list(address)
 	" from " PANTRYC_SQLITE__TABLE_ADDRESS
 	" where " PANTRYC_SQLITE__COLUMN_ADDRESS_REJECTED_RECEIPT " = '%s';",
 			address);
-	int data;
-	sprintf(failure,
-			"**ERROR**		Cannot get rejected receipt address list, %s\n", error);
+	sprintf(failure, "Cannot get rejected receipt address list");
+	int data = 0;
+
 	int result =
-			pantryc_sqlite__execute(pantryc_sqlite__database, sql,
+			pantryc_sqlite__execute(sql,
 					( {	int $(void *data, int size, char **value, char **column) {
 									*((int*) data) = size;
 									return 0;
@@ -144,20 +140,20 @@ static void pantryc_sqlite__initialize(table, column)
 
 	/* No such table */
 	if (result != SQLITE_OK) {
+		fprintf(pantryc_global__log_file, "**NOTICE**\tNo such table \"%s\"\n",
+				table);
 		/* Create table "Address" */
 		if (strcmp(table, PANTRYC_SQLITE__TABLE_ADDRESS) == 0) {
-			sprintf(sql,
-					"create table %s (" PANTRYC_SQLITE__COLUMN_ADDRESS_REJECTED_RECEIPT
-					" char(" PANTRYC_SQLITE__TEXT_LENGTH ") primary key not null);",
-					table);
+			sprintf(sql,/*
+			 "create table %s (" PANTRYC_SQLITE__COLUMN_ADDRESS_REJECTED_RECEIPT
+			 " char(" PANTRYC_SQLITE__TEXT_LENGTH ") unique);",*/
+			"create table %s (tmp int);", table);
 		} else
 			return;
 
-		sprintf(success, "**WARNING**	Create table \"%s\"\n", table);
-		sprintf(failure, "**ERROR**		Cannot create table \"%s\", %s\n", table,
-				error);
-		pantryc_sqlite__execute(pantryc_sqlite__database, sql, NULL, NULL,
-				success, failure, &error);
+		sprintf(success, "Create table \"%s\"", table);
+		sprintf(failure, "Cannot create table \"%s\"", table);
+		pantryc_sqlite__execute(sql, NULL, NULL, success, failure, &error);
 	}
 
 	/* Check column existing */
@@ -165,36 +161,35 @@ static void pantryc_sqlite__initialize(table, column)
 	result = sqlite3_exec(pantryc_sqlite__database, sql, NULL, NULL, &error);
 	/* No such column */
 	if (result != SQLITE_OK) {
+		fprintf(pantryc_global__log_file, "**NOTICE**\tNo such column \"%s\"\n",
+				column);
 		/* Add column "RejectedReceipt" */
 		if (strcmp(column, PANTRYC_SQLITE__COLUMN_ADDRESS_REJECTED_RECEIPT)
 				== 0) {
 			sprintf(sql, "alter table " PANTRYC_SQLITE__TABLE_ADDRESS
-			" add column %s char(" PANTRYC_SQLITE__TEXT_LENGTH
-			") primary key not null );", column);
+			" add column %s char(" PANTRYC_SQLITE__TEXT_LENGTH ");", column); // still not unique
 		} else
 			return;
 
-		sprintf(success, "**WARNING**	Add column \"%s\""
-				" on table \"%s\"\n", column, table);
-		sprintf(failure, "**ERROR**		Cannot add column \"%s\", %s\n", column,
-				error);
-		pantryc_sqlite__execute(pantryc_sqlite__database, sql, NULL, NULL,
-				success, failure, &error);
+		sprintf(success, "Add column \"%s\""
+				" on table \"%s\"", column, table);
+		sprintf(failure, "Cannot add column \"%s\"", column);
+		pantryc_sqlite__execute(sql, NULL, NULL, success, failure, &error);
 	}
 	return;
 }
 
-static int pantryc_sqlite__execute(database, sql, callback, data, success,
-		failure, error)
-	sqlite3* database;const char *sql;int (*callback)(void*, int, char**,
-			char**);void *data;char *success;char *failure;char **error; {
-	int result = sqlite3_exec(pantryc_sqlite__database, sql, NULL, NULL, error);
+static int pantryc_sqlite__execute(sql, callback, data, success, failure, error)
+	const char *sql;int (*callback)(void*, int, char**, char**);void *data;char *success;char *failure;char **error; {
+	int result = sqlite3_exec(pantryc_sqlite__database, sql, callback, data,
+			error);
 	if (result == SQLITE_OK) {
 		if (success != NULL)
-			fprintf(pantryc_global__log_file, "%s", success);
+			fprintf(pantryc_global__log_file, "**SUCCESS**\t%s\n", success);
 	} else {
 		if (failure != NULL)
-			fprintf(pantryc_global__log_file, "%s", failure);
+			fprintf(pantryc_global__log_file, "**ERROR**\t%s, %s\n", failure,
+					*error);
 		sqlite3_free(*error);
 	}
 	return result;
